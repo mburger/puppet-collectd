2892e6f8812875564608b1896c6ccdfaed45feee
Markus Burger <markus.burger@uni-ak.ac.at>
2013-12-27 16:33:35 +0100

    fixed the tail plugins
    Change-Id: I5c81bcc32d88347150be5ad6cdd539fb9c566167

--- manifests/plugin/exim.pp
+++ manifests/plugin/exim.pp
@@ -7,6 +7,7 @@ class collectd::plugin::exim ( ) {
   concat::fragment { 'collectd.tail.exim.conf':
     target  => "${conf_dir}/tail.conf",
     content => template('collectd/exim.conf.erb'),
+    order   => 10,
   }
 
 }
