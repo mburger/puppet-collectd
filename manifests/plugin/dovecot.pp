741068821d522d6329f02bd11e7aa3c20965fdc0
Markus Burger <markus.burger@uni-ak.ac.at>
2013-12-27 10:31:56 +0100

    use concat to emit the tail based plugins for dovecot and exim
    Change-Id: I5d3662ae785b102133817dbbc0bc3e722e9ab989

--- manifests/plugin/dovecot.pp
+++ manifests/plugin/dovecot.pp
@@ -1,12 +1,12 @@
-class collectd::plugin::exim ( ) {
+class collectd::plugin::dovecot ( ) {
   include collectd::params
   include collectd::plugin::tail
 
   $conf_dir = $collectd::params::plugin_conf_dir
 
-  concat::fragment { 'collectd.tail.exim.conf':
+  concat::fragment { 'collectd.tail.dovecot.conf':
     target  => "${conf_dir}/tail.conf",
-    content => template('collectd/exim.conf.erb'),
+    content => template('collectd/dovecot.conf.erb'),
   }
 
 }
