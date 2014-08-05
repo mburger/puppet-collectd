858550d08e41c93cb93944abb1b1a88c4d9793cf
David Schmitt <david@dasz.at>
2014-08-05 09:32:38 +0200

    Add python support

---
+++ manifests/plugin/python.pp
@@ -0,0 +1,24 @@
+class collectd::plugin::python {
+  include collectd::params
+
+  $conf_dir = $collectd::params::plugin_conf_dir
+
+  concat { "${conf_dir}/python.conf":
+    owner  => root,
+    group  => root,
+    mode   => 644,
+    notify => Service['collectd']
+  }
+
+  concat::fragment { 'collectd.python.header.conf':
+    target  => "${conf_dir}/python.conf",
+    content => template("collectd/python.header.conf.erb"),
+    order   => 1,
+  }
+
+  concat::fragment { 'collectd.python.footer.conf':
+    target  => "${conf_dir}/python.conf",
+    content => template("collectd/python.footer.conf.erb"),
+    order   => 8,
+  }
+}
